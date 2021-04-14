db04d2093c6a1fe8f5ba42d25744bc2c803346f6
ABDIRASHID NUUR <78458279+abdinuur10@users.noreply.github.com>
2021-04-13 17:13:08 -0700

    Aniga

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import {Route, Switch } from "react-router-dom"
 import axios from 'axios';
-import StarsList from "./components/StarList"
-
+import StarList from "./components/StarList";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [star, setStar] = useState([]); 
+
+  useEffect(() => {
+    const getStars = () => { 
+      axios
+        .get('https://swapi.dev/')
+        .then(response => {
+          console.log(response)
+          setMovieList(response.data)
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    getStars();
+  }, []);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -16,6 +31,16 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Switch>
+
+      <Route path="/StarList/:starID"> 
+          <MovieCard star = {StarList}/>
+        </Route>
+        <Route path="/" >
+          <MovieList star = {StarList}/>
+        </Route>
+
+      </Switch>
     </div>
   );
 }
--- starwars/src/components/StarList.js
+++ starwars/src/components/StarList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {link, Router, Switch} from "react-router-dom"
 
-export default function MovieList(props) {
+export default function StarList(props) {
     const { Stars } = props
 
     return (
@@ -11,17 +11,10 @@ export default function MovieList(props) {
             <div className="nav-links">
             </div>
           </nav>
-          {/*  Use Link from React Router Dom to make each movie clickable */}
           {movies.map(Stars => (
-            <Link to={`/movieList/${Stars.id}`}>
-            <div className="movie-card"> 
+            <Link to={`/StarList/${Stars.id}`}>
+            <div className="Stars-list"> 
               <h2>{star.title}</h2>
-              <div className="movie-director">
-                Director: <em>{stars.director}</em>
-              </div>
-              <div className="movie-metascore">
-                Metascore: <strong>{starts.metascore}</strong>
-              </div>
             </div>
     
             </Link>
